741051c28ae17a54e074a3d2cb05dbeb5edfaef0
pmira-pe <pmira-pe@student.42madrid.com>
2023-03-16 20:44:18 +0100

    Logger done

--- 02/ex02/logger.py
+++ 02/ex02/logger.py
@@ -3,12 +3,30 @@ from random import randint
 import os
 
 
+def log(func):
+	def wrapper(*args, **kwargs):
+		start_time = time.time()
+		result = func(*args, **kwargs)
+		execution_time = time.time() - start_time
+		func_name = func.__name__.replace('_', ' ').title()
+		with open('machine.log', 'a') as f:
+			if execution_time < 1:
+				time_unit = 'ms'
+				execution_time *= 1000
+			else:
+				time_unit = 's '
+			f.write(f'(cmaxime)Running: {func_name:18} [ exec-time = {execution_time:.3f} ' + time_unit + ' ]\n')
+		return result
+	return wrapper		
+
+#logging.info(f"cmaxime - {func.__name__} - Execution time: {execution_time:.5f} seconds")
+
 
 class CoffeeMachine():
 	
 	water_level = 100
 	
-	#@log
+	@log
 	def start_machine(self):
 		if self.water_level > 20:
 			return True
@@ -16,11 +34,11 @@ class CoffeeMachine():
 			print("Please add water!")
 			return False
 	
-	#@log
+	@log
 	def boil_water(self):
 		return "boiling..."
 	
-	#@log
+	@log
 	def make_coffee(self):
 		if self.start_machine():
 			for _ in range(20):
@@ -29,14 +47,18 @@ class CoffeeMachine():
 			print(self.boil_water())
 			print("Coffee is ready!")
 	
-	#@log
+	@log
 	def add_water(self, water_level):
 		time.sleep(randint(1, 5))
 		self.water_level += water_level
 		print("Blub blub blub...")
 
 if __name__ == "__main__":
-	
+	try:
+		os.remove("machine.log")
+	except FileNotFoundError:
+		pass
+
 	machine = CoffeeMachine()
 	
 	for i in range(0, 5):
